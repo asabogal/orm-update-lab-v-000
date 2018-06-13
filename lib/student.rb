7aa6cdca70620047474b7a8b480201a62119939e
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-13 17:59:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,8 @@ class Student
   end
 
   def self.create_table
-    sql = "CREATE TABLE IF NOT EXISTS  students;"
+    sql = "CREATE TABLE IF NOT EXISTS students;"
+    DB[:conn].execute(sql)
   end
 
 end
