41c4ef13980acabaaf9ab49afca5c319424cc508
Alejandro Sabogal <asabogal.git@gmail.com>
2018-06-13 18:14:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,12 +27,16 @@ class Student
   end
 
   def save
+      if self.id
+        self.update
+      else
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
       SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
+  end
   end
 
 end
